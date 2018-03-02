a6ee4f7a1e28294404098033b2fe11a5bc80998c
erip <25800139+erip1@users.noreply.github.com>
2018-03-02 15:30:31 -0500

    Add files via upload

--- 101/Solution.java
+++ 101/Solution.java
@@ -10,5 +10,16 @@
 class Solution {
     public boolean isSymmetric(TreeNode root) {
         
+        return root == null || sym(root.left, root.right);
+    }
+    
+    public boolean sym(TreeNode left, TreeNode right) {
+        if (left == null || right == null) {
+			return left == right;
+		}
+		if (left.val != right.val) {
+			return false;
+		}
+		return sym(left.left, right.right) && sym(left.right, right.left);
     }
 }
